f60691871cc4b247457ec94132fc23237b324515
RedSpiderMkV <redspidermkv@hotmail.com>
2019-12-05 23:42:54 +0000

    stupid train wifi, can't do jack shit

---
+++ src/RedSpiderTech.FinanceNews/RedSpiderTech.FinanceNews.Host/Utilities/Implementation/AppConfigurationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace RedSpiderTech.FinanceNews.Host.Utilities.Implementation
+{
+    public class AppConfigurationManager
+    {
+        #region Properties
+
+        public string LogFile
+        {
+            get
+            {
+                string logFileDirectory = ConfigurationManager.AppSettings["logFileDirectory"];
+                string logFileName = ConfigurationManager.AppSettings["logFileName"];
+                string logFile = Path.Combine(logFileDirectory, logFileName);
+
+                return logFile;
+            }
+        }
+
+        #endregion
+    }
+}
